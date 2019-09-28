2f3ae6dda4e7aa4b9b7bb9c1c0b5313a2a5dacd1
fsousa <fernandosousa.ti@gmail.com>
2019-09-28 15:41:28 -0300

    Atualização AndroidStudio, Kotlin e API. Kotlin 1.3.41; API 28. Utilização de Kotlinx

--- app/src/main/java/br/com/fernandosousa/lmsapp/TelaInicialActivity.kt
+++ app/src/main/java/br/com/fernandosousa/lmsapp/TelaInicialActivity.kt
@@ -11,6 +11,8 @@ import android.support.v7.widget.SearchView
 import android.support.v7.widget.Toolbar
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_tela_inicial.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 
 class TelaInicialActivity : DebugActivity() {
@@ -32,14 +34,11 @@ class TelaInicialActivity : DebugActivity() {
         Toast.makeText(context, "Parâmetro: $nome", Toast.LENGTH_LONG).show()
         Toast.makeText(context, "Numero: $numero", Toast.LENGTH_LONG).show()
 
-        val mensagem = findViewById<TextView>(R.id.mensagemInicial)
-        mensagem.text = "Bem vindo $nome"
+        mensagemInicial.text = "Bem vindo $nome"
 
-        val botaoSair = findViewById<Button>(R.id.botaoSair)
         botaoSair.setOnClickListener {cliqueSair()}
 
         // colocar toolbar
-        var toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
 
