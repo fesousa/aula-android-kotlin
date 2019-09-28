c3bc132ec52968f3f8063ed50cd611874f7cb8d6
fsousa <fernandosousa.ti@gmail.com>
2019-09-28 17:04:57 -0300

    Atualização AndroidStudio, Kotlin e API. Kotlin 1.3.41; API 28. Utilização de Kotlinx

--- app/src/main/java/br/com/fernandosousa/lmsapp/TelaInicialActivity.kt
+++ app/src/main/java/br/com/fernandosousa/lmsapp/TelaInicialActivity.kt
@@ -89,9 +89,9 @@ class TelaInicialActivity : DebugActivity(), NavigationView.OnNavigationItemSele
     // configuraçao do navigation Drawer com a toolbar
     private fun configuraMenuLateral() {
         // ícone de menu (hamburger) para mostrar o menu
-        var toogle = ActionBarDrawerToggle(this, layourMenuLateral, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        var toogle = ActionBarDrawerToggle(this, layoutMenuLateral, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
 
-        layourMenuLateral.addDrawerListener(toogle)
+        layoutMenuLateral.addDrawerListener(toogle)
         toogle.syncState()
 
         menu_lateral.setNavigationItemSelectedListener(this)
@@ -123,7 +123,7 @@ class TelaInicialActivity : DebugActivity(), NavigationView.OnNavigationItemSele
         }
 
         // fecha menu depois de tratar o evento
-        layourMenuLateral.closeDrawer(GravityCompat.START)
+        layoutMenuLateral.closeDrawer(GravityCompat.START)
         return true
     }
 
