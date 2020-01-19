4dcd9a2c0d2ee86006cbe8c9ca1f4987f76d63eb
fsousa <fernandosousa.ti@gmail.com>
2020-01-19 19:59:18 -0300

    Atualização androidx

--- app/src/main/java/br/com/fernandosousa/lmsapp/DisciplinaCadastroActivity.kt
+++ app/src/main/java/br/com/fernandosousa/lmsapp/DisciplinaCadastroActivity.kt
@@ -1,10 +1,9 @@
 package br.com.fernandosousa.lmsapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_cadastro_disciplina.*
 
-class DisciplinaCadastroActivity : AppCompatActivity() {
+class DisciplinaCadastroActivity : DebugActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
