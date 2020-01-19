14f966d6da29639dee6892e3f3657e76d970721c
fsousa <fernandosousa.ti@gmail.com>
2020-01-19 16:22:08 -0300

    Atualização Kotlin X

--- app/src/main/java/br/com/fernandosousa/lmsapp/MainActivity.kt
+++ app/src/main/java/br/com/fernandosousa/lmsapp/MainActivity.kt
@@ -1,6 +1,6 @@
 package br.com.fernandosousa.lmsapp
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import kotlinx.android.synthetic.main.login.*
