02575f112e970311133fbef2d053e3f7c46218aa
fsousa <fernandosousa.ti@gmail.com>
2020-01-19 16:44:38 -0300

    Atualização androidx

--- app/src/main/java/br/com/fernandosousa/lmsapp/TelaInicialActivity.kt
+++ app/src/main/java/br/com/fernandosousa/lmsapp/TelaInicialActivity.kt
@@ -7,8 +7,8 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
-import android.support.v7.widget.SearchView
-import android.support.v7.widget.Toolbar
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_tela_inicial.*
